f117dbb4b28db4a70ad73f5cae085664621d8608
IsDevOps <issyoseghale@gmail.com>
2024-06-25 23:15:42 +0000

    commited

--- terraform-argocd/main.tf
+++ terraform-argocd/main.tf
@@ -1,26 +1,15 @@
 resource "kubernetes_namespace" "argocd" {
   metadata {
-    name = "argocd"
+    name = var.argocd_namespace
   }
 }
 
-resource "helm_release" "argocd" {
-  name       = "argocd"
-  namespace  = kubernetes_namespace.argocd.metadata[0].name
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  values     = [
-    <<EOF
-    server:
-      service:
-        type: NodePort
-      resources:
-        requests:
-          cpu: "100m"
-          memory: "128Mi"
-        limits:
-          cpu: "500m"
-          memory: "256Mi"
-    EOF
-  ]
+resource "kubernetes_manifest" "argocd_install" {
+  manifest = {
+    "apiVersion" = "v1"
+    "kind" = "Namespace"
+    "metadata" = {
+      "name" = var.argocd_namespace
+    }
+  }
 }
